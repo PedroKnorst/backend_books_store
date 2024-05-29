b17b09cc8b921c0db70e2626b1040f0e8cb77a68
Pedro Henrique Knorst <pedro.knorst@gx2.com.br>
2024-05-29 19:36:38 -0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
